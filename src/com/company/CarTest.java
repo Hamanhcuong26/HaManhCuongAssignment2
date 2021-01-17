20fd7bcd105c8985b8ce65bcc0e2e2531402fd12
Hamanhcuong26 <superluperduper69@gmail.com>
2021-01-17 22:58:03 +0800

    commit assignment 2\

---
+++ src/com/company/CarTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CarTest {
+    public void testMove(){
+        //com.company.Road road = new com.company.Road(8);
+        //com.company.Car car = new com.company.Car(road);
+        //car.move();
+    }
+
+}
